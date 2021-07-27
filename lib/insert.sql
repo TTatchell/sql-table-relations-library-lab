947684b0900d7cadaff85f2fa50625e1a1296da7
TTatchell <tim.tatchell24@gmail.com>
2021-07-27 18:18:57 +1000

    progress

---
+++ lib/insert.sql
@@ -0,0 +1,42 @@
+INSERT INTO series (title)
+VALUES ("asdf"),
+    ("asdfasdf");
+INSERT INTO books (title)
+VALUES ("d"),
+    ("d"),
+    ("d"),
+    ("d"),
+    ("d"),
+    ("d");
+INSERT INTO characters (name)
+VALUES ("d"),
+    ("d"),
+    ("d"),
+    ("d"),
+    ("d"),
+    ("d"),
+    ("d"),
+    ("d");
+INSERT INTO subgenres (name)
+VALUES ("asdf"),
+    ("asdfasdf");
+INSERT INTO authors (name)
+VALUES ("sdf"),
+    ("fdsfdssdf");
+INSERT INTO character_books (book_id, character_id)
+VALUES (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2),
+    (1, 2);
