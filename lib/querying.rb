a2bb2bd4da59955e2e6ceb0d300743bc9650c5e5
TTatchell <tim.tatchell24@gmail.com>
2021-07-28 11:33:42 +1000

    upload

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,19 +12,35 @@ def select_name_and_motto_of_char_with_longest_motto
   ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
 end
 
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species) DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM series
+  INNER JOIN subgenres ON series.subgenre_id = subgenres.id
+  INNER JOIN authors ON series.author_id = authors.id
+  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  INNER JOIN authors ON series.author_id = authors.id
+  INNER JOIN characters ON authors.id = characters.author_id
+  GROUP BY characters.species = 'human' LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(characters.name)
+  FROM books
+  INNER JOIN character_books ON books.id = character_books.book_id
+  INNER JOIN characters ON character_books.character_id = characters.id
+  GROUP BY characters.id
+  ORDER BY COUNT(characters.name) DESC;"
 end
